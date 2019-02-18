9fd4beaad93237de305cc0708f0528b8bb7562a4
lincan <gd_lincan@126.com>
2019-02-18 14:56:16 +0800

    test mybatis

--- src/test/java/learn/ssm/spring/MyBatisTest.java
+++ src/test/java/learn/ssm/spring/MyBatisTest.java
@@ -5,7 +5,10 @@ import learn.ssm.mapper.PetStoreMapper;
 import learn.ssm.pojo.Dog;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Date;
 
 /**
  * @author : Lin Can
@@ -15,25 +18,20 @@ public class MyBatisTest {
     private SqlSessionFactory sqlSessionFactory = MyBatisUtil.getSqlSessionFactory();
 
     @Test
-    public void test1() {
+    public void testMybatisSaveAndQuery() {
         try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
             PetStoreMapper petStoreMapper = sqlSession.getMapper(PetStoreMapper.class);
             Dog dog = new Dog();
-            dog.setName("funny");
+            Date date = new Date();
+            dog.setName(date.toString());
             dog.setAge(5);
-            dog.setOwner("Mary");
+            dog.setOwner("Mary" + date.toString());
             petStoreMapper.insertDog(dog);
             sqlSession.commit();// 这里一定要提交，不然数据进不去数据库中
-        }
-    }
 
-    @Test
-    public void test2() {
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            PetStoreMapper petStoreMapper = sqlSession.getMapper(PetStoreMapper.class);
-            Dog dog = petStoreMapper.getDogByName("funny");
-            System.out.println("name: " + dog.getName() + "|age: "
-                    + dog.getAge());
+            Dog queryDog = petStoreMapper.getDogByName(dog.getName());
+            Assert.assertEquals("name: " + dog.getName() + "|age: " + dog.getAge() + "|owner:" + dog.getOwner(),
+                    "name: " + queryDog.getName() + "|age: " + queryDog.getAge() + "|owner:" + queryDog.getOwner());
         }
     }
 }
